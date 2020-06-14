641a8f541c5bfc131988d6debc0f3cbd71fc2ec9
Vinicius Harres <vsharres@gmail.com>
2020-06-14 14:00:53 -0300

    Delete config_dev.js

--- config/config_dev.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	prefix: '<:devitos:721584982514204743>',
-	height: 147, // in cm
-	weight: 48, // in Kg
-	token : 'NzIxNTI2Mzg2ODc1MTcwODQ2.XuV02g.vJJYjpkf_4jfjPZSoDayCBJ--ig',
-};
