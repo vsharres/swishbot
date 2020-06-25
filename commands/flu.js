aebc6c03295cd6ca8061ad7d68e450f7c51d61af
Vinicius Harres <vinicius.harres@g.globo>
2020-06-25 17:52:08 -0300

    Update to the flu message

--- commands/flu.js
+++ commands/flu.js
@@ -17,7 +17,9 @@ module.exports = {
                 {
                     member.createDM()
                     .then(channel=> {
-                        channel.send(`Hello Swisher! \nAs a surprise thank you gift to the hosts of Swish and Flick on their anniversary, we thought it would be fun to all leave messages on what they mean to us! If you’d like to participate, click the link below.\nhttps://docs.google.com/forms/d/1MQAA7uw2wV_pUPtF6OUy9NiCz0vJd-LkCACT497ygK4/edit?usp=sharing`)
+                        channel.send(`Hello Swisher! \nAs a surprise thank you gift to the hosts of Swish and Flick on their anniversary, we thought it would be fun to all leave messages on what they mean to us! 
+                        If you’d like to participate, click the link below. 
+                        Don't forget this is supposed to be a SURPRISE, please don't discuss in the discord chat.\nThe dealine for submitting a message is July 30th! \nhttps://docs.google.com/forms/d/1MQAA7uw2wV_pUPtF6OUy9NiCz0vJd-LkCACT497ygK4/edit?usp=sharing`)
                                 .catch(err=>console.error(err));
                 })
                     .catch(err=>console.error(err));
