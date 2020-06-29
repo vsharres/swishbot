edd2fba06f0f60d6202f5e266ec8f98a0fce2d83
Vinicius Harres <vinicius.harres@g.globo>
2020-06-29 13:00:10 -0300

    added the id

--- commands/flu.js
+++ commands/flu.js
@@ -1,7 +1,5 @@
 
-const mia_id =0;
-const marchismo_id=0;
-const brandon_id=0; 
+const {mia_id,marchismo_id,brandon_id,vini_id} = require('../config/configs');
 
 module.exports = {
     name: "flu!",
@@ -23,7 +21,7 @@ module.exports = {
                 {
                     member.createDM()
                     .then(channel=> {
-                        channel.send(`Hello Swisher!\nJust a reminder, we are planning a surprise thank you gift to the hosts of Swish and Flick on their anniversary. We thought it would be fun to all leave messages on what they mean to us!\nWe have had some hiccups with the google form we are using to collect answers. If you run into an issue where the form isn't working OR you need to edit or resubmit your response please privately direct message: @mia-gilli @marchismo @Brandon Seabaugh or @ViniciusHarres.\nIf you want to participate, click the link below. Don't forget this is supposed to be a **SURPRISE**, ~~please don't discuss in the general discord chat~~. The deadline for submitting a message is **July 15th!**\nhttps://forms.gle/2EZRaUJQsymPHgEZ8`)
+                        channel.send(`Hello Swisher!\nJust a reminder, we are planning a surprise thank you gift to the hosts of Swish and Flick on their anniversary. We thought it would be fun to all leave messages on what they mean to us!\nWe have had some hiccups with the google form we are using to collect answers. If you run into an issue where the form isn't working OR you need to edit or resubmit your response please privately direct message: ${members.get(mia_id)} ${members.get(marchismo_id)} ${members.get(brandon_id)} or ${members.get(vini_id)}.\nIf you want to participate, click the link below. Don't forget this is supposed to be a **SURPRISE**, __please don't discuss in the general discord chat__. The deadline for submitting a message is **July 15th!**\nhttps://forms.gle/2EZRaUJQsymPHgEZ8`)
                                 .catch(err=>console.error(err));
                 })
                     .catch(err=>console.error(err));
--- config/config_prod.js
+++ config/config_prod.js
@@ -6,5 +6,9 @@ module.exports = {
 	binger_gif: 'https://media.giphy.com/media/JPsFUPp3vLS5q/giphy.gif',
 	height: 147, // in cm
 	weight: 48, // in Kg
+	mia_id:process.env.MIA_ID,
+	marchismo_id:process.env.MARCHISMO_ID,
+	brandon_id:process.env.BRANDON_ID,
+	vini_id:process.env.VINI_ID,
 	token : process.env.DISCORD_TOKEN,
 };
