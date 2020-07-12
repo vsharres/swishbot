885c7e4426f6c16a1b877fc3aacbde7f3bd7ddde
Vinicius Harres <vinicius.harres@g.globo>
2020-07-12 18:33:05 -0300

    change to the devito converter to use switch, faster than if-else

--- commands/devitos.js
+++ commands/devitos.js
@@ -14,45 +14,68 @@ module.exports = {
         if (args.length === 1) {
             let parsed = args.shift();
             let multiplier = 1;
-            let unit = "cm";
+            let unit = parsed.replace(/[0-9]/g, '');
 
-            if (parsed.includes('m') || parsed.includes('meters') || parsed.includes('meter')) {
-                multiplier = 100;
-                unit = "m";
-            }
-            else if (parsed.includes('cm') || parsed.includes('centimeters') || parsed.includes('centimeter')) {
-                multiplier = 1;
-                unit = "cm";
-            }
-            else if (parsed.includes('km') || parsed.includes('kilometers') || parsed.includes('kilometer')) {
-                multiplier = 100000;
-                unit = "km";
-            }
-            else if (parsed.includes('yd') || parsed.includes('yards') || parsed.includes('yard')) {
-                multiplier = 91.44;
-                unit = "yd";
+            //for the special case of using ' or "
+            const rex = /^(?!$|.*\'[^\x22]+$)(?:([0-9]+)\')?(?:([0-9]+)\x22?)?$/;
+            const match = rex.exec(parsed);
+            if (match) {
+                let feet = parseFloat(match[1]) ? parseFloat(match[1]) : 0;
+                let inches = parseFloat(match[2]) ? parseFloat(match[2]) : 0;
+
+                const devitos = (((feet * 30.48) + (inches * 2.54)) / config.height).toFixed(7);
+                return message.channel.send(`${message.author} ${feet > 0 ? feet + "'" : ''}${inches > 0 ? inches + '"' : ''} is ${devitos} ${prefix}!`);
 
             }
-            else if (parsed.includes('mi') || parsed.includes('miles') || parsed.includes('mile')) {
-                multiplier = 160934;
-                unit = "mi";
-            }
-            else if (parsed.includes("'") || parsed.includes('"')) {
 
-                const rex = /^(?!$|.*\'[^\x22]+$)(?:([0-9]+)\')?(?:([0-9]+)\x22?)?$/;
-                const match = rex.exec(parsed);
-                if (match) {
-                    let feet = parseFloat(match[1]) ? parseFloat(match[1]) : 0;
-                    let inches = parseFloat(match[2]) ? parseFloat(match[2]) : 0;
-
-                    const devitos = (((feet * 30.48) + (inches * 2.54)) / config.height).toFixed(7);
-                    return message.channel.send(`${message.author} ${feet > 0 ? feet + "'" : ''}${inches > 0 ? inches + '"' : ''} is ${devitos} ${prefix}!`);
-
-                }
-
-            }
-            else {
-                return message.channel.send(`${message.author} the proper usage would be: ${prefix} \`${this.name} ${this.usage}\``);
+            switch (unit) {
+                case 'm':
+                    multiplier = 100;
+                    break;
+                case 'meters':
+                    multiplier = 100;
+                    break;
+                case 'meter':
+                    multiplier = 100;
+                    break;
+                case 'cm':
+                    multiplier = 1;
+                    break;
+                case 'centimeters':
+                    multiplier = 1;
+                    break;
+                case 'centimeter':
+                    multiplier = 1;
+                    break;
+                case 'km':
+                    multiplier = 100000;
+                    break;
+                case 'kilometers':
+                    multiplier = 100000;
+                    break;
+                case 'kilometer':
+                    multiplier = 100000;
+                    break;
+                case 'yd':
+                    multiplier = 91.44;
+                    break;
+                case 'yard':
+                    multiplier = 91.44;
+                    break;
+                case 'yards':
+                    multiplier = 91.44;
+                    break;
+                case 'mi':
+                    multiplier = 160934;
+                    break;
+                case 'mile':
+                    multiplier = 160934;
+                    break;
+                case 'miles':
+                    multiplier = 160934;
+                    break;
+                default:
+                    return message.channel.send(`${message.author} the proper usage would be: ${prefix} \`${this.name} ${this.usage}\``);
             }
 
             parsed = parsed.substring(0, parsed.length - unit.length);
@@ -72,29 +95,66 @@ module.exports = {
             amount = parseFloat(args.shift());
             unit = args.shift();
 
-            if (unit == 'cm' || unit === 'centimeters' || unit === 'centimeter') {
-                multiplier = 1;
-            }
-            else if (unit === 'm' || unit === 'meters' || unit === 'meter') {
-                multiplier = 100;
-            }
-            else if (unit === 'km' || unit === 'kilometers' || unit === 'kilometer') {
-                multiplier = 100000;
-            }
-            else if (unit === 'mi' || unit === 'miles' || unit === 'mile') {
-                multiplier = 160934;
-            }
-            else if (unit === 'yd' || unit === 'yards' || unit === 'yard') {
-                multiplier = 91.44;
-            }
-            else if (unit === 'inches' || unit === 'inch') {
-                multiplier = 2.54;
-            }
-            else if (unit === 'feet' || unit === 'foot') {
-                multiplier = 30.48;
-            }
-            else {
-                return message.channel.send(`${message.author} the proper usage would be: ${prefix} \`${this.name} ${this.usage}\``);
+            switch (unit) {
+                case 'cm':
+                    multiplier = 1;
+                    break;
+                case 'centimeters':
+                    multiplier = 1;
+                    break;
+                case 'centimeter':
+                    multiplier = 1;
+                    break;
+                case 'm':
+                    multiplier = 100;
+                    break;
+                case 'meter':
+                    multiplier = 100;
+                    break;
+                case 'meters':
+                    multiplier = 100;
+                    break;
+                case 'km':
+                    multiplier = 100000;
+                    break;
+                case 'kilometer':
+                    multiplier = 100000;
+                    break;
+                case 'kilometers':
+                    multiplier = 100000;
+                    break;
+                case 'mi':
+                    multiplier = 160934;
+                    break;
+                case 'mile':
+                    multiplier = 160934;
+                    break;
+                case 'miles':
+                    multiplier = 160934;
+                    break;
+                case 'yd':
+                    multiplier = 91.44;
+                    break;
+                case 'yard':
+                    multiplier = 91.44;
+                    break;
+                case 'yards':
+                    multiplier = 91.44;
+                    break;
+                case 'inches':
+                    multiplier = 2.54;
+                    break;
+                case 'inch':
+                    multiplier = 2.54;
+                    break;
+                case 'feet':
+                    multiplier = 30.48;
+                    break;
+                case 'foot':
+                    multiplier = 30.48;
+                    break;
+                default:
+                    return message.channel.send(`${message.author} the proper usage would be: ${prefix} \`${this.name} ${this.usage}\``);
             }
 
             let devitos = (amount * multiplier / config.height).toFixed(7);
