11de421c33e61b251ee439d499c4a95e774aec2e
Vinicius Harres <vinicius.harres@g.globo>
2020-08-12 19:26:47 -0300

    new format for the house points

--- index.js
+++ index.js
@@ -406,31 +406,106 @@ async function printPoints(message, points) {
     }
 
     let houses = [{
-        name: 'Gryffindor 🦁',
+        house: 'Gryffindor 🦁',
         points: points.gryffindor
     },
     {
-        name: 'Slytherin 🐍',
+        house: 'Slytherin 🐍',
         points: points.slytherin
     },
     {
-        name: 'Ravenclaw 🦅',
+        house: 'Ravenclaw 🦅',
         points: points.ravenclaw
     },
     {
-        name: 'Hufflepuff 🦡',
+        house: 'Hufflepuff 🦡',
         points: points.hufflepuff
     }
     ];
 
+    const gryf = houses[0].points;
+    const slyth = hosues[1].points;
+    const raven = houses[2].points;
+    const huff = houses[4].points;
+
     houses.sort((a, b) => b.points - a.points);
     let reply = '**House Points**\n\n';
 
-    houses.forEach(house => {
-        reply += `${house.name} with a total of **${house.points}!**\n\n`;
-    });
+    //Check if any house is tied
+    if (gryf === slyth || gryf === raven || gryf === huff || slyth === raven || slyth === huff || raven === huff) {
 
-    return hourglass_channel.send(reply);
+        //for the case when all houses are tied
+        if (gryf === slyth && slyth === raven && raven === huff) {
+
+            reply += `All houses are tied with **${gryf} points!**`;
+            return hourglass_channel.send(reply);
+
+        }
+        //for the case with 3 houses tied
+        else if ((gryf === slyth && slyth === raven) || (gryf === slyth && slyth === huff) || (gryf === raven && raven === huff) || (slyth === raven && raven === huff)) {
+            if (houses[0] === houses[1]) {
+                reply += `${houses[0].house}, ${houses[1].house}, ${houses[2].house} are tied in first place with **${houses[0].points} points!**\n\n`;
+                reply += `${houses[3].house} is in second place with **${houses[3].points} points!**\n`;
+            }
+            else {
+                reply += `${houses[0].house} is in first place with **${houses[0].points} points!**\n`;
+                reply += `${houses[1].house}, ${houses[2].house}, ${houses[3].house} are tied in second place with **${houses[1].points} points!**\n\n`;
+            }
+
+            return hourglass_channel.send(reply);
+        }
+        //for the case where only two houses are tied
+        else {
+            if (houses[0] === houses[1]) {
+                reply += `${houses[0].house}, ${houses[1].house} are tied in first place with **${houses[0].points} points!**\n`;
+                reply += `${houses[2].house} is in second place with **${houses[2].points} points!**\n`;
+                reply += `${houses[3].house} is in third place with **${houses[3].points} points!**\n\n`;
+
+                return hourglass_channel.send(reply);
+            }
+            else if (houses[1] === houses[2]) {
+                reply += `${houses[0].house} is in first place with **${houses[0].points} points!**\n`;
+                reply += `${houses[1].house}, ${houses[2].house} are tied in second place with **${houses[1].points} points!**\n`;
+                reply += `${houses[3].house} is in third place with **${houses[3].points} points!**\n\n`;
+
+                return hourglass_channel.send(reply);
+            }
+            else {
+                reply += `${houses[0].house} is in first place with **${houses[0].points} points!**\n`;
+                reply += `${houses[1].house} is in second place with **${houses[1].points} points!**\n`;
+                reply += `${houses[2].house}, ${houses[3].house} are tied in third place with **${houses[2].points} points!**\n\n`;
+
+                return hourglass_channel.send(reply);
+            }
+        }
+    }
+    else {
+
+        for (let index = 0; index < houses.length; index++) {
+            let placement = '';
+            switch (index) {
+                case 0:
+                    placement = 'first';
+                    break;
+                case 1:
+                    placement = 'second';
+                    break;
+                case 2:
+                    placement = 'third';
+                    break;
+                case 3:
+                    placement = 'fourth';
+                    break;
+                default:
+                    break;
+            }
+            reply += `${houses[index].house} is in ${placement} place with **${houses[index].points} points!**\n`;
+
+        }
+        reply += '\n';
+        return hourglass_channel.send(reply);
+
+    }
 }
 
 function sleep(ms) {
