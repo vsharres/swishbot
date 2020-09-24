6e762fd6dd441d0a3094f2f61fde6c582171c9d7
Vinicius Harres <vinicius.harres@g.globo>
2020-09-23 21:26:10 -0300

    added command to reset the lightning bolts

---
+++ commands/lightning_reset.ts
@@ -0,0 +1,35 @@
+import Stat, { Lightning } from '../models/Stat';
+import { Configs } from '../config/configs';
+import { Message } from 'discord.js';
+import { Logger } from 'winston';
+import { Command } from './command';
+
+export class LightningReset extends Command {
+
+    constructor() {
+        super(["lightning_reset", "reset_lightning"], 'Reset all of the lightning bolts', 10, '', false, false, true);
+    }
+
+    async execute(message: Message, arg: string[], logger: Logger) {
+
+        Stat.findById(Configs.stats_id).then(stat => {
+
+            if (!stat) {
+                return logger.log('error', 'Error getting the stat, check the stat id');
+            }
+
+            stat.lightnings = new Array<Lightning>();
+
+            stat
+                .save()
+                .then(() => {
+                    logger.log('info', `All of the lightning bolts are reset.`);
+                })
+                .catch(err => logger.log('error', err));
+
+        })
+            .catch(err => logger.log('error', err));
+    }
+};
+
+export default new LightningReset();
