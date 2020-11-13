c3e528f7a667c97cb4917543d291cd09e6a31dc5
Vinicius Harres <vinicius.harres@g.globo>
2020-11-12 21:47:51 -0300

    removing the likes

--- index.ts
+++ index.ts
@@ -37,7 +37,7 @@ handlers.set('points', new Points(logger));
 handlers.set('commands', new Commands(logger));
 handlers.set('votes', new Votes(logger));
 handlers.set('kicks', new Kicks(logger));
-handlers.set('likes', new Likes(logger));
+//handlers.set('likes', new Likes(logger));
 
 client.once('ready', () => {
     logger.log('info', 'Ready!');
@@ -107,39 +107,12 @@ client.on('messageReactionAdd', async (reaction, user) => {
             return;
         }
     }
-    handlers.get('votes')?.OnReaction(user as User, reaction);
+    handlers.forEach(async handler => {
+        handler.OnReaction(user as User, reaction);
+    });
 
 });
 
-//NEED TO CHANGE THAT
-//Checking for reactions
-client.on('messageReactionAdd', async (reaction, user) => {
-
-    if (reaction.partial) {
-
-        try {
-            await reaction.fetch();
-        }
-        catch (error) {
-            logger.log('error', `[Index]: Something went wrong when fetching the message: ${error}`);
-            return;
-        }
-    }
-
-    if (user.partial) {
-
-        try {
-            await user.fetch();
-
-        }
-        catch (error) {
-            logger.log('error', `[Index]: Something went wrong when fetching the user: ${error}`);
-            return;
-        }
-    }
-    handlers.get('points')?.OnReaction(user as User, reaction);
-
-});
 
 process.on('uncaughtException', error => logger.log('error', error));
 
