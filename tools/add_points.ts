5bf0e7ab0cc05e90b23ae282474eb79d2ccc46a5
Vinicius Harres <vinicius.harres@g.globo>
2020-12-16 18:39:49 -0300

    improving performance of the add points function

--- tools/add_points.ts
+++ tools/add_points.ts
@@ -14,27 +14,27 @@ export function addPoints(points: number, house_points: Houses, member: GuildMem
 
     let pointsToAdd: Houses = { gryffindor: 0, slytherin: 0, ravenclaw: 0, hufflepuff: 0 };
 
-    if (memberRoles.has(Configs.role_gryffindor)) {
-        pointsToAdd.gryffindor += points * Configs.gryffindor_points_multiplier;
-    }
+    memberRoles.forEach(role => {
+        if (Configs.role_gryffindor === role.id) {
+            pointsToAdd.gryffindor += points * Configs.gryffindor_points_multiplier;
+        }
 
-    if (memberRoles.has(Configs.role_slytherin)) {
-        pointsToAdd.slytherin += points * Configs.slytherin_points_multiplier;
-    }
+        if (Configs.role_slytherin === role.id) {
+            pointsToAdd.slytherin += points * Configs.slytherin_points_multiplier;
+        }
 
-    if (memberRoles.has(Configs.role_ravenclaw)) {
-        pointsToAdd.ravenclaw += points * Configs.ravenclaw_points_multiplier;
-    }
+        if (Configs.role_ravenclaw === role.id) {
+            pointsToAdd.ravenclaw += points * Configs.ravenclaw_points_multiplier;
+        }
 
-    if (memberRoles.has(Configs.role_hufflepuff)) {
-        pointsToAdd.hufflepuff += points * Configs.hufflepuff_points_multiplier;
-    }
-
+        if (Configs.role_hufflepuff === role.id) {
+            pointsToAdd.hufflepuff += points * Configs.hufflepuff_points_multiplier;
+        }
+    });
 
     house_points.gryffindor += pointsToAdd.gryffindor;
     house_points.ravenclaw += pointsToAdd.ravenclaw;
     house_points.slytherin += pointsToAdd.slytherin;
     house_points.hufflepuff += pointsToAdd.hufflepuff;
-
     return house_points;
 }
