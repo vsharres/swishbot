659902666e5f94f9be79a0293d033d61f5e3eeef
Vinicius Harres <vinicius.harres@g.globo>
2021-02-04 23:17:52 -0300

    removing the chart

--- tools/chart_points.ts
+++
@@ -1,6 +0,0 @@
-import { CanvasRenderService } from 'chartjs-node-canvas';
-
-const pointsCanvasRender = new CanvasRenderService(900, 600);
-
-export async function renderPointsCanvas(configuration: any) { return pointsCanvasRender.renderToBuffer(configuration) }
-
