b5df841e3e817ce810e86ba0afa2d33bcb316de1
Vinicius Harres <vinicius.harres@g.globo>
2021-02-06 00:07:23 -0300

    Added a function to reset the funny array

---
+++ commands/funny_reset.ts
@@ -0,0 +1,35 @@
+import Stat, { Funny } from '../models/Stat';
+import { Configs } from '../config/configs';
+import { Message } from 'discord.js';
+import logger from '../tools/logger';
+import { Command } from './command';
+
+export class FunnyReset extends Command {
+
+    constructor() {
+        super(["funny_reset", "reset_funny"], false, false, true);
+    }
+
+    async execute(message: Message, arg: string[]) {
+
+        Stat.findById(Configs.stats_id).then(stat => {
+
+            if (!stat) {
+                return logger.log('error', `[${this.names[0]}]: Error getting the stat, check the stat id`);
+            }
+
+            stat.funnies = new Array<Funny>();
+
+            stat
+                .save()
+                .then(() => {
+                    logger.log('info', `[${this.names[0]}]: All of the funny moments are reset`);
+                })
+                .catch(err => logger.log('error', `[${this.names[0]}]: ${err}`));
+
+        })
+            .catch(err => logger.log('error', `[${this.names[0]}]: ${err}`));
+    }
+};
+
+export default new FunnyReset();
