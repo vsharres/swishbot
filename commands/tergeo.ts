6e73bac872aea20ff8a10c5cb041acb81775fc4d
Vinicius Harres <vinicius.harres@g.globo>
2021-12-11 00:04:18 -0300

    added the command to prune the patrons

---
+++ commands/tergeo.ts
@@ -0,0 +1,49 @@
+import { Client, Guild, GuildMemberManager, Message } from 'discord.js';
+import logger from '../tools/logger';
+import { Command } from './command';
+import { Configs } from '../config/configs';
+
+export class Tergeo extends Command {
+
+    guild: Guild;
+
+    constructor(client: Client) {
+        super(client, ['tergeo'], true);
+
+        this.guild = client.guilds.cache.get(Configs.guild_id) as Guild;
+        
+    }
+
+    async execute(message: Message, arg: string[]) {
+
+       const Members = await this.guild.members.fetch();
+
+       Members.forEach((member) => {
+
+        if (!member.roles.cache.has(Configs.role_prefect) && !member.roles.cache.has(Configs.role_admin)){
+            
+            if (member.roles.cache.has(Configs.role_phoenix_emoji)) {
+                const is_phoenix_and_up = member.roles.cache.has(Configs.role_phoenix) || member.roles.cache.has(Configs.role_unicorn) || member.roles.cache.has(Configs.role_thunderbird);
+
+                if (!is_phoenix_and_up) {
+                    member.roles.remove(Configs.role_phoenix_emoji);
+                    logger.log('info', `[${this.names[0]}]: ${member.displayName} phoenix role was removed`);
+                }
+
+            }
+        
+            const is_no_longer_patron = !member.roles.cache.has(Configs.role_patron);
+            const is_dragon_and_up = member.roles.cache.has(Configs.role_phoenix) || member.roles.cache.has(Configs.role_unicorn) || member.roles.cache.has(Configs.role_thunderbird) || member.roles.cache.has(Configs.role_dragon);
+
+            if ((!is_no_longer_patron || !is_dragon_and_up) && member.roles.cache.has(Configs.role_ageline)) {
+                member.roles.remove(Configs.role_ageline);
+                logger.log('info', `[${this.names[0]}]: ${member.displayName} age line role removed as the user no longer has access.`);
+            }
+        }
+
+       });
+    }
+
+};
+
+export default (client: Client) => { return new Tergeo(client) };
