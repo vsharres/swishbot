c46eeb0a6946f862f7f1f2fcade0cbd847b2addb
Vinicius Harres <vinicius.harres@g.globo>
2022-08-18 19:26:54 -0300

    fix the plebs command

--- commands/plebs.ts
+++ commands/plebs.ts
@@ -9,23 +9,29 @@ export class Plebs extends Command {
     recording_channel: TextChannel;
 
     constructor(client: Client) {
-        super(client, ["plebs", "pleb"], false, false, true);
+        super(client, ["plebs", "pleb"], true, false, true);
 
         this.recording_channel = client.channels.cache.get(Configs.channel_recording) as TextChannel;
     }
 
-    async execute(message: Message, arg: string[]) {
+    async execute(message: Message, args: string[]) {
 
         Stat.findById(Configs.stats_id).then((stat) => {
             if (!stat) {
                 return logger.log('error', `[${this.names[0]}]: Error getting the stat, check the stat id`);
             }
 
+            let plebMessage = args.join();
+            if (!plebMessage) {
+                logger.log('error', `[${this.names[0]}]: Error shifting the house.`);
+                return;
+            }
+
             let plebs_message = `@everyone a recording will start shortly!** \n`;
 
             this.recording_channel
                             .send(plebs_message)
-                            .then(() => logger.log('info', `@everyone a recording will start shortly!** \n`))
+                            .then(() => logger.log('info', `${plebMessage}`))
                             .catch(err => logger.log('error', `[${this.names[0]}]: ${err}`));
 
         })
