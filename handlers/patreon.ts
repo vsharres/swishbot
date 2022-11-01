ffef526b507db08cb31ced79d5725760ff387f6a
Vinicius Harres <vsharres@gmail.com>
2022-11-01 16:00:26 -0300

    updating the patreon roles

--- handlers/patreon.ts
+++ handlers/patreon.ts
@@ -17,19 +17,19 @@ export class Patreon extends Handler {
         }
 
         if (newMember.roles.cache.has(Configs.role_phoenix_emoji)) {
-            const is_phoenix_and_up = newMember.roles.cache.has(Configs.role_phoenix) || newMember.roles.cache.has(Configs.role_unicorn) || newMember.roles.cache.has(Configs.role_thunderbird);
+            const is_dragon_and_up = newMember.roles.cache.has(Configs.role_dragon) || newMember.roles.cache.has(Configs.role_phoenix)
 
-            if (!is_phoenix_and_up) {
+            if (!is_dragon_and_up) {
                 newMember.roles.remove(Configs.role_phoenix_emoji);
-                logger.log('info', `[${this.name}]: ${newMember.displayName} phoenix role was removed from patron.`);
+                logger.log('info', `[${this.name}]: ${newMember.displayName} dragon role was removed from patron.`);
             }
 
         }
         
         const is_patron = newMember.roles.cache.has(Configs.role_patron);
-        const is_dragon_and_up = newMember.roles.cache.has(Configs.role_phoenix) || newMember.roles.cache.has(Configs.role_unicorn) || newMember.roles.cache.has(Configs.role_thunderbird) || newMember.roles.cache.has(Configs.role_dragon);
+        const is_hippogriff_and_up = newMember.roles.cache.has(Configs.role_phoenix) || newMember.roles.cache.has(Configs.role_dragon) || newMember.roles.cache.has(Configs.role_hippogriff);
 
-        if ((!is_patron || !is_dragon_and_up) && newMember.roles.cache.has(Configs.role_ageline)) {
+        if ((!is_patron || !is_hippogriff_and_up) && newMember.roles.cache.has(Configs.role_ageline)) {
             newMember.roles.remove(Configs.role_ageline);
             logger.log('info', `[${this.name}]: ${newMember.displayName} age line role removed as the user no longer has access.`);
         }
