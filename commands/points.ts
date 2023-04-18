12330d71ec5cebda4c45d173d772af6596dfd084
Vinicius Harres <vsharres@gmail.com>
2023-04-18 18:15:50 -0300

    removing the double reference to house points from the points command

--- commands/points.ts
+++ commands/points.ts
@@ -28,7 +28,7 @@ export class Points extends Command {
 
             printPoints(interaction.channel as TextChannel, stat.points);
 
-            return await interaction.reply({ content: `House points`, ephemeral: false })
+            return await interaction.reply({ content: '', ephemeral: true })
 
         })
             .catch(err => logger.log('error', `[${this.name}]: ${err}`));
